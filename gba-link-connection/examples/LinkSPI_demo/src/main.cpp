582fe469f9ad001b949090b91fc3bf15fe276c52
copyrat90 <copyrat90@gmail.com>
2023-05-14 19:57:00 +0900

    Alter `LinkSPI_demo` to multiboot into slave mode

--- gba-link-connection/examples/LinkSPI_demo/src/main.cpp
+++ gba-link-connection/examples/LinkSPI_demo/src/main.cpp
@@ -27,77 +27,23 @@ void init() {
 int main() {
   init();
 
-  bool firstTransfer = false;
-  bool async = false;
-  u32 counter = 0;
+  bool firstTransfer = true;
+
+  linkSPI->activate(LinkSPI::Mode::SLAVE);
 
   while (true) {
-    std::string output = "";
+    std::string output = "[gba-pico-gamepad]\n\n";
     u16 keys = ~REG_KEYS & KEY_ANY;
 
-    if (!linkSPI->isActive()) {
-      firstTransfer = true;
-      output += "START: Set as Master\n";
-      output += "SELECT: Set as Slave\n";
-      output += "\n(stop: press L+R)\n";
-      output += "(hold A on start for async)\n";
-      output += "(hold B on start for waitMode)\n";
-      output +=
-          "\n\n\n\n\n\n\n\n\n\n\n\n[!] to test this demo...\n      "
-          "...use a GBC Link Cable!";
-
-      if ((keys & KEY_START) | (keys & KEY_SELECT)) {
-        // (3) Initialize the library
-        linkSPI->activate((keys & KEY_START) ? LinkSPI::Mode::MASTER_256KBPS
-                                             : LinkSPI::Mode::SLAVE);
-        linkSPI->setWaitModeActive(keys &
-                                   KEY_B);  // see `waitMode` in README.md
-        if (keys & KEY_A)
-          async = true;
-      }
-    } else {
-      // Title
-      auto modeName =
-          linkSPI->getMode() == LinkSPI::Mode::SLAVE ? "[slave]" : "[master]";
-      output += std::string(modeName) + "\n";
-      if (firstTransfer) {
-        log(output + "Waiting...");
-        firstTransfer = false;
-      }
-
-      if (!async) {
-        // (4)/(5) Exchange 32-bit data with the other end
-        u32 remoteKeys = linkSPI->transfer(keys, []() {
-          u16 keys = ~REG_KEYS & KEY_ANY;
-          return (keys & KEY_L) && (keys & KEY_R);
-        });
-        output += "> " + std::to_string(keys) + "\n";
-        output += "< " + std::to_string(remoteKeys) + "\n";
-      } else {
-        // (6) Exchange data asynchronously
-        if (keys != 0 &&
-            linkSPI->getAsyncState() == LinkSPI::AsyncState::IDLE) {
-          counter++;
-          linkSPI->transferAsync(counter, []() {
-            u16 keys = ~REG_KEYS & KEY_ANY;
-            return (keys & KEY_L) && (keys & KEY_R);
-          });
-          log(output + ">> " + std::to_string(counter));
-          wait(228 * 60);
-        }
-        if (linkSPI->getAsyncState() == LinkSPI::AsyncState::READY) {
-          log(output + "<< " + std::to_string(linkSPI->getAsyncData()));
-          wait(228 * 60);
-        }
-      }
-
-      // Cancel
-      if ((keys & KEY_L) && (keys & KEY_R)) {
-        linkSPI->deactivate();
-        async = false;
-        counter = 0;
-      }
+    if (firstTransfer) {
+      log(output + "Waiting...");
+      firstTransfer = false;
     }
+
+    // Exchange 32-bit data with the other end
+    u32 remoteKeys = linkSPI->transfer(keys);
+    output += "send: " + std::to_string(keys) + "\n";
+    output += "recv: " + std::to_string(remoteKeys) + "\n";
 
     // Print
     VBlankIntrWait();
